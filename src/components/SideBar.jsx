5d96f2d5fbb8fd0780661e6942c43e13edcd6dca
nuovvvva <kanmengnuo@outlook.com>
2025-04-19 16:40:32 +0800

    A Great mistake: commit repo without ignoring the env file so I lost all the commit histories

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function SideBar(props) {
+  const {handleToggleModal, data} = props;
+
+  return (
+    <div onClick={handleToggleModal} className="sidebar">
+      <div className="bgOverlay"></div>
+      <div className="sidebarContents">
+        <h2>{data?.title}</h2>
+        <div className="descriptionContainer">
+          <p className="descriptionTitle">{data?.date}</p>
+          <p>{data?.explanation}</p>
+        </div>
+        <button onClick={handleToggleModal}>
+          <i className="fa-solid fa-right-long"></i>
+        </button>
+      </div>
+
+    </div>
+
+  )
+}
